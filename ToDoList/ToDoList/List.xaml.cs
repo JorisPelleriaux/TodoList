e0f09c412afc4485355cdf5a8bbe5d2bbc37225d
RomanTymofiyev <roman.tymofiyev@gmail.com>
2015-03-16 11:51:25 +0100

    List.xaml + List.xaml.cs aangemaakt

---
+++ ToDoList/ToDoList/List.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ToDoList
+{
+    public partial class List : PhoneApplicationPage
+    {
+        public List()
+        {
+            InitializeComponent();
+        }
+
+        private void addTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (toDoBox.Text != "")
+            {
+                toDoList.Items.Add(toDoBox.Text + " " + choseDate.ValueString + " " + choseTime.ValueString);
+                toDoBox.Text = "";
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter a text");
+            }
+        }
+
+        private void toDoBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
