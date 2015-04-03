8e15edeafa5e7eda082721b632d69f04eb266de4
jorisp <joris_pelleriaux5@hotmail.com>
2015-04-03 11:13:04 +0200

    fout gemaakt (reverted from commit 1cdb6861b152abcee86d78f3e942b7bfc435cb18)

--- nieuwe start/KidsList/KidsList.WindowsPhone/MainPage.xaml.cs
+++ nieuwe start/KidsList/KidsList.WindowsPhone/MainPage.xaml.cs
@@ -4,11 +4,6 @@ namespace KidsList
 {
     public sealed partial class MainPage : Page
     {
-        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ParentRegister));
-        }
-
         private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             Frame.Navigate(typeof(ParentRegister));
